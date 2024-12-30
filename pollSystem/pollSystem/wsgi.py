56c60a9dbd8fdaead7208e301affe29378409e87
Jefferson Luiz Seno <jeffersonseno@gmail.com>
2024-12-30 18:16:20 -0300

    Treinando Django com sistema de enquetes

--- pollSystem/pollSystem/wsgi.py
+++ pollSystem/pollSystem/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Polls project.
+WSGI config for pollSystem project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Polls.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pollSystem.settings')
 
 application = get_wsgi_application()
